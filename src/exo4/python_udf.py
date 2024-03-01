cb3e4078b8a510dbe5f14a3c4534a08a9d929d5a
Flaco924 <rom1.dehaese@gmail.com>
2024-03-01 17:55:26 +0100

    ajout main func

--- src/exo4/python_udf.py
+++ src/exo4/python_udf.py
@@ -1,23 +1,26 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
+from pyspark.sql.functions import col, udf
 from pyspark.sql.types import StringType, IntegerType
-from pyspark.sql.column import Column, _to_java_column, _to_seq
 
-spark = SparkSession.builder.appName("UDF").master("local[*]").config('spark.jars', 'src/resources/exo4/udf.jar').getOrCreate()
+def main():
+    spark = SparkSession.builder \
+        .appName("UDF") \
+        .master("local[*]") \
+        .config('spark.jars', 'src/resources/exo4/udf.jar') \
+        .getOrCreate()
 
-data_path = "src/resources/exo4/sell.csv/*"
-df = spark.read.option("header", "true").csv(data_path)
-df = df.withColumn("category", col("category").cast(IntegerType()))
+    data_path = "src/resources/exo4/sell.csv"
+    df = spark.read.option("header", "true").csv(data_path)
+    df = df.withColumn("category", col("category").cast(IntegerType()))
 
-# Définition de la fonction UDF
-def categorize_category(category):
-    if category < 6:
-        return "food"
-    else:
-        return "furniture"
+    def categorize_category(category):
+        if category < 6:
+            return "food"
+        else:
+            return "furniture"
 
-# Enregistrer la fonction UDF
-categorize_category_udf = udf(categorize_category, StringType())
+    categorize_category_udf = udf(categorize_category, StringType())
+    df = df.withColumn("category_name", categorize_category_udf(df["category"]))
 
-# Ajouter une nouvelle colonne en utilisant l'UDF
-df = df.withColumn("category_name", categorize_category_udf(df["category"]))
+if __name__ == "__main__":
+    main()
--- src/exo4/scala_udf.py
+++ src/exo4/scala_udf.py
@@ -1,22 +1,29 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import *
-from pyspark.sql.types import StringType, IntegerType
+from pyspark.sql.functions import col
 from pyspark.sql.column import Column, _to_java_column, _to_seq
 
-spark = SparkSession.builder.appName("UDF").master("local[*]").config('spark.jars', 'src/resources/exo4/udf.jar').getOrCreate()
+def main():
+    spark = SparkSession.builder \
+        .appName("UDF") \
+        .master("local[*]") \
+        .config('spark.jars', 'src/resources/exo4/udf.jar') \
+        .getOrCreate()
 
-data_path = "src/resources/exo4/sell.csv/*"
-df = spark.read.option("header", "true").csv(data_path)
-df = df.withColumn("category", col("category").cast(IntegerType()))
+    data_path = "src/resources/exo4/sell.csv"
+    df = spark.read.option("header", "true").csv(data_path)
+    df = df.withColumn("category", col("category").cast(IntegerType()))
 
-# Définir la fonction Python pour utiliser l'UDF Scala
-def addCategoryName(col):
-    # on récupère le SparkContext
-    sc = spark.sparkContext
-    # Via sc._jvm on peut accéder à des fonctions Scala
-    add_category_name_udf = sc._jvm.fr.hymaia.sparkfordev.udf.Exo4.addCategoryNameCol()
-    # On retourne un objet colonne avec l'application de notre udf Scala
-    return Column(add_category_name_udf.apply(_to_seq(sc, [col], _to_java_column)))
+    # Définir la fonction Python pour utiliser l'UDF Scala
+    def addCategoryName(col):
+        # Récupérer le SparkContext
+        sc = spark.sparkContext
+        # Via sc._jvm, accéder aux fonctions Scala
+        add_category_name_udf = sc._jvm.fr.hymaia.sparkfordev.udf.Exo4.addCategoryNameCol()
+        # Retourner un objet colonne avec l'application de l'UDF Scala
+        return Column(add_category_name_udf.apply(_to_seq(sc, [col], _to_java_column)))
 
-# Ajouter la nouvelle colonne en utilisant l'UDF Scala
-df = df.withColumn("category_name", addCategoryName(col("category")))
+    # Ajouter la nouvelle colonne en utilisant l'UDF Scala
+    df = df.withColumn("category_name", addCategoryName(col("category")))
+
+if __name__ == "__main__":
+    main()
