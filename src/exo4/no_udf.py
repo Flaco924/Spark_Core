1f0805a83973ebe0831e675b79f782ab196ec5fc
Flaco924 <rom1.dehaese@gmail.com>
2024-03-04 14:55:53 +0100

    change write path

--- src/exo4/no_udf.py
+++ src/exo4/no_udf.py
@@ -17,7 +17,7 @@ def main():
 
     # Ajouter la nouvelle colonne avec des conditions
     df = df.withColumn("category_name", when(col("category") < 6, "food").otherwise("furniture"))
-    df.write.csv('src/resources/outputs_exo4/no_udf_result.csv')
+    df.write.csv('src/outputs_exo4/no_udf_result.csv')
 
     end_time = time.time()
     execution_time = end_time - start_time
